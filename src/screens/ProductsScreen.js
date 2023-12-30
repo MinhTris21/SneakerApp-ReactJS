95436636ff2da280c18382a2c905e989a135e740
MinhTris <boyds_24854@yahoo.com.vn>
2023-12-30 13:27:24 +0700

    init + home screen

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from '../data/products';
+const ProductsScreen = () =>{
+    return(
+      <FlatList
+        data={products}
+        renderItem ={({ item }) => (
+      <View style = {styles.itemcontainer}>
+      <Image source={{uri: item.image,}} style ={styles.image}/>
+      </View>
+      )}
+      numColumns = {2}
+    />
+    )
+}
+const styles = StyleSheet.create({
+    itemcontainer:{
+      width: "50%",
+      padding: 1,
+    },
+    image: {
+      width: "100%", 
+      aspectRatio: 1,
+    }
+  });
+
+  export default ProductsScreen;
